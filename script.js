1486204b834955ef95ca5d7b60d190cb47049470
joyce <joycemusungi2002@gmail.com>
2026-07-31 08:57:09 +0300

    created a variable named dayOfTheWeek

---
+++ script.js
@@ -0,0 +1,18 @@
+const dayOfTheWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+const maleNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];

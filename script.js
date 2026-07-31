ba17308000a65034bdc26c5c40784a4386e9e279
joyce <joycemusungi2002@gmail.com>
2026-07-31 13:09:19 +0300

    feat:add the DOM elements in the file

--- script.js
+++ script.js
@@ -25,3 +25,8 @@ const femaleName = [
     "Afua",
     "Ama",
 ];
+const akanForm = document.getElementById("akanForm");
+const resultContainer = document.getElementById("resultContainer");
+const resultName = document.getElementById("resultName");
+const resultDetails = document.getElementById("resultDetails");
+const resetBtn = docment.getElementById("resetBtn");

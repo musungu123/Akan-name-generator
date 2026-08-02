8fc487875d65eb0320ee32b58da0ce0c0dd18493
joyce <joycemusungi2002@gmail.com>
2026-08-02 23:12:47 +0300

    faet:edditted the script.js file

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-const dayOfTheWeek = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-];
-const maleNames = [
-    "Kwasi",
-    "Kwadwo",
-    "Kwabena",
-    "Kwaku",
-    "Yaw",
-    "Kofi",
-    "kwame",
-];
-const femaleName = [
-    "Akosua",
-    "Adwoa",
-    "Abenaa",
-    "Akua",
-    "Yaa",
-    "Afua",
-    "Ama",
-];
-const akanForm = document.getElementById("akanForm");
-const resultContainer = document.getElementById("resultContainer");
-const resultName = document.getElementById("resultName");
-const resultDetails = document.getElementById("resultDetails");
-const resetBtn = docment.getElementById("resetBtn");
-/**
- * Akan Name Generator Logic
- */
 
 // Data mapping: Index corresponds to calculated day (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
 const daysOfWeek = [
